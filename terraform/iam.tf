d2992a0a6b5d5dae114c51f86eb1b50342a8121c
ovidiu <ovidiu.andreescu1212@gmail.com>
2025-09-19 17:25:14 +0300

    fixed remotive api call

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,7 +2,7 @@ data "aws_iam_policy_document" "assume_lambda" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
@@ -12,34 +12,22 @@ resource "aws_iam_role" "lambda" {
   name               = var.prefix != "" ? "${var.prefix}-job-agg-lambda-role" : "job-agg-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.assume_lambda.json
 }
-# match all versions/stages of this secret
-# CloudWatch Logs
+
 resource "aws_iam_role_policy_attachment" "cwlogs" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# SecretsManager least-privilege
-data "aws_iam_policy_document" "secrets" {
-  statement {
-    sid     = "ReadSecretsManager"
-    actions = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
-    resources = local.secret_arn_patterns != [] ? local.secret_arn_patterns : ["*"]
-  }
-
-  dynamic "statement" {
-    for_each = var.kms_key_id == null ? [] : [1]
-    content {
-      sid       = "KMSDecryptForSecrets"
-      actions   = ["kms:Decrypt"]
-      resources = [var.kms_key_id]
-    }
-  }
-}
-
 resource "aws_iam_policy" "secrets" {
   name   = var.prefix != "" ? "${var.prefix}-job-agg-secrets-read" : "job-agg-secrets-read"
-  policy = data.aws_iam_policy_document.secrets.json
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Action    = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"],
+      Resource  = local.secret_arn_patterns
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_secrets" {
@@ -47,32 +35,25 @@ resource "aws_iam_role_policy_attachment" "attach_secrets" {
   policy_arn = aws_iam_policy.secrets.arn
 }
 
-data "aws_iam_policy_document" "dynamodb_jobs_write" {
-  statement {
-    sid = "AllowWriteToJobsTable"
-    actions = [
-      "dynamodb:PutItem",
-      "dynamodb:UpdateItem",
-      "dynamodb:BatchWriteItem",
-      "dynamodb:DescribeTable",
-      "dynamodb:GetItem",
-      "dynamodb:Query",
-      "dynamodb:BatchGetItem"
-    ]
-
-    resources = [
-      aws_dynamodb_table.jobs.arn,
-      "${aws_dynamodb_table.jobs.arn}/index/*"
-    ]
-  }
-}
-
+# Custom policy for writing to the DynamoDB jobs table
 resource "aws_iam_policy" "dynamodb_jobs_write" {
   name   = var.prefix != "" ? "${var.prefix}-job-agg-dynamodb-write" : "job-agg-dynamodb-write"
-  policy = data.aws_iam_policy_document.dynamodb_jobs_write.json
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "dynamodb:PutItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:BatchWriteItem"
+      ],
+      Resource = aws_dynamodb_table.jobs.arn
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "attach_dynamodb_write" {
   role       = aws_iam_role.lambda.name
   policy_arn = aws_iam_policy.dynamodb_jobs_write.arn
 }
+
